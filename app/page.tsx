a368487b82a6c6cfadf3d93917476b4b0e9ba49b
Nishant Menon <nishantsmenon@gmail.com>
2024-11-26 23:31:42 -0600

    dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React, { useState } from "react";
+import * as React from "react"
+import { Moon, MoonIcon, Sun } from "lucide-react"
 import {
   Accordion,
   AccordionContent,
@@ -19,10 +20,18 @@ import {
 } from "@/components/ui/carousel"
 
 import { Switch } from "@/components/ui/switch"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
+import { useTheme } from "next-themes"
 
 
 export default function Home() {
+  const { setTheme } = useTheme()
 
   return (
     <div className="p-8 space-y-16 bg-background text-foreground">
@@ -104,6 +113,30 @@ export default function Home() {
     <h1>Airplane Mode</h1>
     <Switch />
   </section>
+
+  <section className="p-8 border border-border rounded-md space-y-4">
+    <h1>Click here to enable Dark Mode!</h1>
+  <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+    </section>
 
 
 
